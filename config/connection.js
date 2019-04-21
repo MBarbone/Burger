3d389a30d97e19cd4cfcab0b457499efde523ffc
Mike <michael.barbone@gmail.com>
2019-04-20 22:43:20 -0400

    Create app routing and create mysql database and connection.

--- config/connection.js
+++ config/connection.js
@@ -1,2 +1,16 @@
 // require mysql
 var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 8080,
+    user: 'root',
+    password: '',
+    database: 'burgers_db'
+});
+
+connection.connect(function(err){
+    if(err) throw err
+});
+
+module.exports = connection;
--- controllers/burger_controllers.js
+++ controllers/burger_controllers.js
@@ -7,7 +7,7 @@ var burger = require('../models/burger');
 
 // define homepage
 router.get('/', function(req, res){
-    burger.all(function(data){
+    burger.selectAll(function(data){
         var hbsBurger = {
             burgers: data
         };
@@ -25,3 +25,37 @@ router.post('/api/burgers', function(req,res){
         res.json({id: result.insertId});
     });
 });
+
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log('condition', condition);
+  
+    burger.update({
+      isDevoured: req.body.isDevoured
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+  
+  router.delete("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    burger.delete(condition, function(result) {
+      if (result.affectedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+  
+  // Export routes for server.js to use.
+  module.exports = router;
+  
